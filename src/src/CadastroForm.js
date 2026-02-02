808946781666a6b2eb3a83c69b807670eab6874a
luiz bandeira <lbandeira@gmail.com>
2026-02-02 11:17:48 -0300

    Troca definitiva de audio para trilha_galatica_v1

--- src/src/CadastroForm.js
+++ src/src/CadastroForm.js
@@ -109,7 +109,7 @@ const CadastroForm = () => {
   // --- AJUSTE DE ÁUDIO AQUI ---
   // Certifique-se que o arquivo existe em: public/sounds/01. Intro_To_the_Stars.mp3
   useEffect(() => {
-    const currentMusic = "/sounds/musica_fundo.mp3";
+    const currentMusic = "/sounds/trilha_galatica_v1.mp3";
     playTrack(currentMusic, { loop: true, isPrimary: false });
   }, [playTrack]);
 
