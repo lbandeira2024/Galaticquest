5a966cab3672b2bedc779b3ffd435a454184a8d6
luiz bandeira <lbandeira@gmail.com>
2026-02-20 14:28:10 -0300

    Initial commit

--- src/DecolagemMarte.js
+++ src/DecolagemMarte.js
@@ -199,7 +199,6 @@ const MainDisplayWindow = React.memo(({
         className="cloud-animation-video"
         autoPlay
         muted
-        loop
         playsInline
         preload="auto"
         style={{
